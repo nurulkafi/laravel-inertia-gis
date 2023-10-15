18208d25b2583f7d0358c48ef9391c568dce33f8
nurulkafi <nurulkafi18@gmail.com>
2023-10-15 21:46:38 +0700

    Add toast for alert message

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { Link, usePage } from "@inertiajs/react";
 import Alerts from "@/Components/FloatUI/Alerts";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
-        const { flash } = usePage().props;
-        console.log("flash", usePage().props);
+    const { flash } = usePage().props;
+    var boolean = 0;
+    // const [modifiedFlash, setModifiedFlash] = useState("");
     const navigation = [
         {
             href: "/dashboard",
@@ -52,7 +57,26 @@ export default function Authenticated({ user, header, children }) {
                 </svg>
             ),
         },
-
+        {
+            href: "/graph",
+            name: "Graph",
+            icon: (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z"
+                    />
+                </svg>
+            ),
+        },
     ];
 
     const navsFooter = [
@@ -122,11 +146,54 @@ export default function Authenticated({ user, header, children }) {
             ),
         },
     ];
+
+    if (flash.type) {
+        if (flash.type) {
+            const commonToastOptions = {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                // onOpen: () => window.alert("Called when I open"),
+            };
+
+            if (flash.type === "success") {
+                toast.success(flash.message, commonToastOptions);
+            }
+            if (flash.type === "error") {
+                toast.error(flash.message, commonToastOptions);
+            }
+
+            flash.type = null;
+            flash.message = null;
+        }
+    }
     return (
         <>
-            {flash.type && flash.message && (
-                <Alerts type={flash.type} message={flash.message} />
-            )}
+            {/* {flash.type && flash.message && (
+                <Alerts
+                    type={flash.type}
+                    message={flash.message}
+                    showAlert={showAlert}
+                    setShowAlert={setShowAlert}
+                />
+            )} */}
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
             <nav className="fixed top-0 left-0 w-full h-full border-r bg-white space-y-8 sm:w-80">
                 <div className="flex flex-col h-full">
                     <div className="h-20 flex items-center px-8">
